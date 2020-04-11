4225936c2eb1ebd531132941831714297e1852d8
Richard Boyd <rhboyd@amazon.com>
2020-04-11 01:48:55 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Cloud9 IDE UserData
+## Build The App
+
+````bash
+REGION=us-west-2
+S3_BUCKET_NAME=$(aws sts get-caller-identity --query "Account" --output text)-${REGION}-$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 16 | head -n 1)
+aws s3 mb s3://${S3_BUCKET_NAME} --region $REGION
+sam build
+sam package -t .aws-sam/build/template.yaml --s3-bucket $S3_BUCKET_NAME --output-template-file packaged.yaml
+sam publish -t packaged.yaml --region $REGION
+````
+Now we'll fetch the ARN of the SAR App
+````bash
+aws serverlessrepo list-applications --query "Applications[?Name == 'Cloud9BootStrapper'].ApplicationId" --region $REGION --output text
+````
+
+## Usage
+````yaml
+AWSTemplateFormatVersion: '2010-09-09'
+Transform: AWS::Serverless-2016-10-31
+
+Resources:
+  MyCloud9Env:
+    Type: AWS::Cloud9::EnvironmentEC2
+    Properties: 
+      InstanceType: t2.micro
+
+  BootStrapper:
+    Type: AWS::Serverless::Application
+    Properties:
+      Location:
+        ApplicationId: [SAR ARN]
+        SemanticVersion: 0.1.0
+      Parameters:
+        Cloud9Environment: !Ref MyCloud9Env
+        EBSVolumeSize: 75
+        UserData: !Base64 |
+          mkdir /home/ec2-user/environment/RichardWasHere
+          touch /home/ec2-user/environment/RichardWasHere/README.md
+          echo "Here is some text" >> /home/ec2-user/environment/RichardWasHere/README.md
+````
---
+++ hello_world/app.py
@@ -0,0 +1,106 @@
+from __future__ import print_function
+import logging
+from time import sleep
+import boto3
+import base64
+from crhelper import CfnResource
+
+logger = logging.getLogger(__name__)
+helper = CfnResource(json_logging=True, log_level='DEBUG', boto_level='CRITICAL')
+
+try:
+    ssm_client = boto3.client('ssm')
+    ec2_client = boto3.client('ec2')
+except Exception as e:
+    helper.init_failure(e)
+
+def associate_profile(instance_id: str, instance_profile: str):
+    logger.debug("Creating Association")
+    associate_response = ec2_client.associate_iam_instance_profile(IamInstanceProfile={'Name': instance_profile},InstanceId=instance_id)
+    response = ec2_client.describe_iam_instance_profile_associations(Filters=[{'Name': 'instance-id','Values': [instance_id]},{'Name': 'state','Values': ['associated']}])
+    logger.debug("Waiting for Association")
+    while len(response['IamInstanceProfileAssociations'])==0:
+        sleep(15)
+        response = ec2_client.describe_iam_instance_profile_associations(Filters=[{'Name': 'instance-id','Values': [instance_id]},{'Name': 'state','Values': ['associated']}])
+    logger.debug("Association Complete")
+    return
+    
+def get_command_output(instance_id, command_id):
+    response = ssm_client.get_command_invocation(CommandId=command_id, InstanceId=instance_id)
+    if response['Status'] in ['Pending', 'InProgress', 'Delayed']:
+        return
+    return response
+
+
+def send_command(instance_id, commands: [str]):
+    logger.debug("Sending command to %s : %s" % (instance_id, commands))
+    try:
+        return ssm_client.send_command(
+            InstanceIds=[instance_id], 
+            DocumentName='AWS-RunShellScript', 
+            Parameters={'commands': commands}
+        )
+    except ssm_client.exceptions.InvalidInstanceId:
+        logger.debug("Failed to execute SSM command", exc_info=True)
+        return
+
+def rezise_ebs(instance_id: str, volume_size: int):
+    instance = ec2_client.describe_instances(Filters=[{'Name': 'instance-id', 'Values': [instance_id]}])['Reservations'][0]['Instances'][0]
+    block_volume_id = instance['BlockDeviceMappings'][0]['Ebs']['VolumeId']
+    ec2_client.modify_volume(VolumeId=block_volume_id,Size=volume_size)
+    return
+
+@helper.create
+def create(event, context):
+    logger.debug("Got Create")
+    environment_id = event["ResourceProperties"]["EnvironmentId"]
+    commands = ['sudo growpart /dev/xvda 1 ||sudo resize2fs /dev/xvda1|| sudo resize2fs /dev/nvme0n1p1'] + base64.b64decode(event["ResourceProperties"]["Commands"]).decode("utf-8").split('\n')
+    size = event['ResourceProperties']['VolumeSize']
+    response = ec2_client.describe_instances(Filters=[{'Name':'tag:aws:cloud9:environment', 'Values': [environment_id]}])
+    logger.debug("response: {}".format(response))
+    instance_id = response['Reservations'][0]['Instances'][0]['InstanceId']
+    helper.Data["InstanceId"] = instance_id
+    try:
+        associate_profile(instance_id=instance_id, instance_profile=event['ResourceProperties']['InstanceProfile'])
+    except Exception as e:
+        raise Exception("Failed to set Associations", e)
+    try:
+        rezise_ebs(instance_id, int(size))
+    except Exception as e:
+        raise Exception("Failed to resize EBS", e)
+    while True:
+        send_response = send_command(instance_id, commands)
+        if send_response:
+            helper.Data["CommandId"] = send_response['Command']['CommandId']
+            logger.debug("response: {}".format(send_response))
+            
+            break
+        if context.get_remaining_time_in_millis() < 20000:
+            raise Exception("Timed out attempting to send command to SSM")
+        sleep(30)
+
+
+@helper.poll_create
+def poll_create(event, context):
+    logger.info("Got create poll")
+    while True:
+        try:
+            cmd_output_response = get_command_output(helper.Data["InstanceId"], helper.Data["CommandId"])
+            if cmd_output_response:
+                break
+        except ssm_client.exceptions.InvocationDoesNotExist:
+            logger.debug('Invocation not available in SSM yet', exc_info=True)
+        if context.get_remaining_time_in_millis() < 20000:
+            return
+        sleep(15)
+    if cmd_output_response['StandardErrorContent']:
+        raise Exception("ssm command failed: " + cmd_output_response['StandardErrorContent'][:235])
+    return helper.Data["InstanceId"]
+
+
+@helper.update
+@helper.delete
+def no_op(_, __):
+    return
+def handler(event, context):
+    helper(event, context)
